5ba08aa13d590f427a81d384cb3cf2b0fa5faefe
liutsing <liutsingluo@gmail.com>
2021-09-19 01:34:43 +0800

    feat: 🎸 add a book

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,7 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
-// @link: https://kaisery.github.io/trpl-zh-cn/ch02-00-guessing-game-tutorial.html
+use rand::Rng; // trait
+use std::cmp::Ordering; // enum
+use std::io; // prelude
+             // @link: https://kaisery.github.io/trpl-zh-cn/ch02-00-guessing-game-tutorial.html
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..101);
@@ -13,6 +13,7 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
         let guess: u32 = match guess.trim().parse() {
+            // enum
             Ok(num) => num,
             Err(e) => {
                 eprintln!("Err: {}! Your should enter valid number!", e);
@@ -21,6 +22,7 @@ fn main() {
         };
         println!("Your guessed {}", guess);
         match guess.cmp(&secret_number) {
+            // enum
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Rust Learning
+
+## lessons
+## projects
+
+## practise website
+- [rust track](https://exercism.org/tracks/rust)
