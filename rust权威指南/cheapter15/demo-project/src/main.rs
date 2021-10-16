f2338ebfe0924692f02cfe79954c577332e60f70
liutsing <liutsingluo@gmail.com>
2021-10-16 23:20:05 +0800

    refactor: 💡 update dir

--- rust权威指南/cheapter15/demo-project/src/main.rs
+++ rust权威指南/cheapter15/demo-project/src/main.rs
@@ -1,41 +1,54 @@
 use crate::List::{Cons, Nil};
-use std::fmt::Debug;
-use std::ops::{Deref, Drop};
-use std::rc::Rc;
+use std::ops::Deref;
+
+fn main() {
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    let x = 5;
+    let y = &x;
+    assert_eq!(5, x);
+    // assert_eq!(5, y); // no implementation for `{integer} == &{integer}`
+
+    let x = 5;
+    let y = Box::new(5);
+    let y2 = MyBox::new(5);
+    println!("{:p}", &x);
+    println!("{:p}", y);
+    // println!("{:p}", y2);
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    assert_eq!(5, *y2);
+
+    let m = MyBox::new(String::from("Rust"));
+    //&m &MyBox<String>
+    // defer &String
+    // defer &str
+    hello(&m);
+
+    hello("Rust");
+
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    drop(c); //手动清理
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+
+    println!("CustomSmartPointer created.");
+    // auto invoke drop
+    // Dropping CustomSmartPointer with data: other stuff
+    // Dropping CustomSmartPointer with data: my stuff
+}
+
+fn hello(name: &str) {
+    println!("{}", name);
+}
 
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(i32, Box<List>),
     Nil,
 }
 
-fn main() {
-    let x = 5;
-    let y = MyBox::new(x);
-    assert_eq!(5, x);
-    assert_eq!(5, *y); // *(y.deref())
-    let m = MyBox::new(String::from("Rust"));
-    hello(&m);
-    drop(m);
-    println!("******************");
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    // let b = Cons(3, Rc::clone(&a)); // 不会执行数据的深度拷贝操作，只增加引用计数
-
-    println!("count after creating a = {}", Rc::strong_count(&a)); //1
-    let b = Cons(6, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a)); //2
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a)); //3
-    }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a)); //2
-    println!("******************");
-}
-
-
-fn hello(name: &str) {
-    println!("Hello, {}!", name);
-}
-#[derive(Debug)]
 struct MyBox<T>(T);
 
 impl<T> MyBox<T> {
@@ -43,16 +56,21 @@ impl<T> MyBox<T> {
         MyBox(x)
     }
 }
-
 impl<T> Deref for MyBox<T> {
     type Target = T;
+
     fn deref(&self) -> &T {
-        &self.0 //不希望获得MyBox<T>内部值的所有权
+        &self.0
     }
 }
 
-impl<T> Drop for MyBox<T> {
+// impl<T> Pointer for MyBox<T> {}
+
+struct CustomSmartPointer {
+    data: String,
+}
+impl Drop for CustomSmartPointer {
     fn drop(&mut self) {
-        println!("Drop droped");
+        println!("Dropping CustomSmartPointer with data: '{}'", self.data);
     }
 }
