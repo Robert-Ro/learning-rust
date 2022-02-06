37f948d0ab295389210530d549f6f1eeabf8cbcc
liutsing <liutsingluo@gmail.com>
2022-02-06 14:21:22 +0800

    add more codewars practises

--- codewars/src/main.rs
+++ codewars/src/main.rs
@@ -1,9 +1,10 @@
 #[allow(unused)]
 use codewars::fundamental::*;
 fn main() {
-    let a = "sdf".chars().enumerate().map(|(i, c)| {
-        c.to_string().to_uppercase() + c.to_string().to_lowercase().repeat(i).as_str()
-    });
-    println!("{:?}", a.collect::<Vec<_>>());
-    println!("{}", 02 / 100 + 1);
+    // let a = "sdf".chars().enumerate().map(|(i, c)| {
+    //     c.to_string().to_uppercase() + c.to_string().to_lowercase().repeat(i).as_str()
+    // });
+    // println!("{:?}", a.collect::<Vec<_>>());
+    // println!("{}", 02 / 100 + 1);
+    // println!("{:?}", tribonacci(&[0., 1., 1.], 10));
 }
