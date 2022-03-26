3b855e86a3c055afb969528f510aa1e4f2e8d567
Liutsing <liutsingluo@163.com>
2022-03-26 09:47:46 +0800

    feat: use sleep to mock effects

--- serve/src/main.rs
+++ serve/src/main.rs
@@ -1,6 +1,8 @@
 use std::fs::read_to_string;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3080").unwrap();
@@ -14,10 +16,16 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024]; // on the stack to hold the data that is read in
     stream.read(&mut buffer).unwrap(); // read and put the them in buffer
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
     let mut status_line;
     let contents;
 
     if buffer.starts_with(get) {
+        status_line = "HTTP/1.1 200 OK";
+        contents = read_to_string("./html/index.html");
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5)); // => Waiting (TTFB)
         status_line = "HTTP/1.1 200 OK";
         contents = read_to_string("./html/index.html");
     } else {
