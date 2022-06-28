4d59f757e07703136f3b0a9bb5c24e9663d6384c
liutsing <liutsingluo@gmail.com>
2022-06-28 10:30:51 +0800

    feat: add html.rs

--- browser-engine/maple_browser_engine/src/html.rs
+++ browser-engine/maple_browser_engine/src/html.rs
@@ -9,33 +9,35 @@ pub fn parse(source: String) -> Vec<dom::Node> {
     .parse_nodes()
 }
 pub struct Parser {
+    /// 游标位置
     pos: usize,
+    /// 输入字符
     input: String,
 }
 
 impl Parser {
-    // Read the current character without consuming it
+    /// Read the current character without consuming it
     fn next_char(&self) -> char {
         self.input[self.pos..].chars().next().unwrap()
     }
-    // Do the next characters start with the given string
+    /// Do the next characters start with the given string
     fn starts_with(&self, s: &str) -> bool {
         self.input[self.pos..].starts_with(s)
     }
-    // Return true if all input is consumed
+    /// Return true if all input is consumed
     fn eof(&self) -> bool {
         self.pos >= self.input.len()
     }
-    // Return the current character, and advance self.pos to the next character.
+    /// Return the current character, and advance self.pos to the next character.
     fn consume_char(&mut self) -> char {
         // char_indices: Returns an iterator over the [`char`]s of a string slice, and their positions
-        let mut iter = self.input[self.pos..].char_indices();
+        let mut iter = self.input[self.pos..].char_indices(); //获取剩余字符的迭代string集合
         let (_, cur_char) = iter.next().unwrap();
         let (next_pos, _) = iter.next().unwrap_or((1, ' '));
         self.pos += next_pos;
         cur_char
     }
-    // Consume characters until `test` returns false.
+    /// Consume characters until `test` returns false.
     fn consume_while<F>(&mut self, test: F) -> String
     where
         F: Fn(char) -> bool,
@@ -46,29 +48,29 @@ impl Parser {
         }
         result
     }
-    // Consume and discard zero or more whitespace characters.
+    /// Consume and discard zero or more whitespace characters.
     fn consume_whitespace(&mut self) {
         self.consume_while(char::is_whitespace);
     }
-    // parse a tag or attribute name
+    /// parse a tag or attribute name
     fn parse_tag_or_attribute_name(&mut self) -> String {
         self.consume_while(|c| match c {
             'a'..='z' | 'A'..='Z' | '0'..='9' => true,
             _ => false,
         })
     }
-    // Parse a single node
+    /// Parse a single node
     fn parse_node(&mut self) -> dom::Node {
         match self.next_char() {
             '<' => self.parse_element(),
             _ => self.parse_text(),
         }
     }
-    // Parse a text node
+    /// Parse a text node
     fn parse_text(&mut self) -> dom::Node {
         dom::text(self.consume_while(|c| c != '<'))
     }
-    // Parse a single element, including its open tag, contents, and closing tag
+    /// Parse a single element, including its open tag, contents, and closing tag
     fn parse_element(&mut self) -> dom::Node {
         assert_eq!(self.consume_char(), '<');
         let tag_name = self.parse_tag_or_attribute_name();
@@ -85,15 +87,16 @@ impl Parser {
 
         return dom::elem(tag_name, attrs, children);
     }
-    // Parse a single name="value" pair.
+    /// Parse a single name="value" pair.
     fn parse_attr(&mut self) -> (String, String) {
         let name = self.parse_tag_or_attribute_name();
+        /// assert!(cond) cond is true
         assert!(self.consume_char() == '=');
         let value = self.parse_attr_value();
         return (name, value);
     }
 
-    // Parse a quoted value.
+    /// Parse a quoted value.
     fn parse_attr_value(&mut self) -> String {
         let open_quote = self.consume_char();
         assert!(open_quote == '"' || open_quote == '\'');
@@ -102,7 +105,7 @@ impl Parser {
         return value;
     }
 
-    // Parse a list of name="value" pairs, separated by whitespace.
+    /// Parse a list of name="value" pairs, separated by whitespace.
     fn parse_attributes(&mut self) -> dom::AttrMap {
         let mut attributes = HashMap::new();
         loop {
@@ -115,7 +118,7 @@ impl Parser {
         }
         attributes
     }
-    // Parse a sequence of sibling nodes.
+    /// Parse a sequence of sibling nodes.
     fn parse_nodes(&mut self) -> Vec<dom::Node> {
         let mut nodes = Vec::new();
         loop {
@@ -127,7 +130,7 @@ impl Parser {
         }
         return nodes;
     }
-    // Parse an HTML document and return the root element.
+    /// Parse an HTML document and return the root element.
     pub fn parse(source: String) -> dom::Node {
         let mut nodes = Parser {
             pos: 0,
